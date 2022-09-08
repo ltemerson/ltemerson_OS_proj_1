d4dda8cb83c448a3d465069e45ea8cea48f86623
Lucas Emerson <lucas.t.emerson@gmail.com>
2022-09-08 15:58:27 -0600

    finished assignment

--- reverse.c
+++ reverse.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 struct Node {
 	char *data;
 	struct Node* next;
 };
 
 void printList(struct Node*);
+void writeToFile(FILE*, struct Node*);
 
 void printList(struct Node* head) {
-	printf("\n[head] =>");
 	while(head != NULL) {
-		printf(" %s =>",head->data);
+		printf("%s",head->data);
 		head = head->next;
 	}
+}
 
-	printf(" [null]\n");
+void writeToFile(FILE* fp, struct Node* head) {
+	while(head !=NULL) {
+		fprintf(fp,"%s",head->data);
+		head = head->next;
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	FILE *file1;
-	FILE *file2
+	FILE *file2;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
@@ -30,10 +36,26 @@ int main(int argc, char *argv[])
 		return(1);
 	}
 	
-
+	
 	if ((file1 = fopen(argv[1],"r")) == NULL) {
 		fprintf(stderr, "error: cannot open file '%s'\n",argv[1]);
 		return(1);
+	}
+	
+	if(argc == 3) {
+		int fp1Cnt = strlen(argv[1]);
+		int fp2Cnt = strlen(argv[2]);
+		if(fp1Cnt == fp2Cnt) {
+			if((strncmp(argv[1],argv[2],fp2Cnt)) == 0) {
+				fprintf(stderr, "error: input and output file must differ\n");
+				return(1);
+			}
+		} 
+
+		if((file2 = fopen(argv[2],"w")) == NULL) {
+			fprintf(stderr, "error: cannot open file '%s'\n",argv[2]);
+			return(1);
+		}
 	}
 	
 	struct Node* head = (struct Node*) malloc(sizeof(struct Node));
@@ -64,9 +86,15 @@ int main(int argc, char *argv[])
 	head = head->next;
 
 	free(curr);
-
-	printList(head);
-
+	
+	if(argc == 2){
+		printList(head);
+	}
+	
+	if(argc == 3) {
+		writeToFile(file2,head);
+	}
+	
 	while(head!=NULL) {
 		curr = head;
 		head= head->next;
@@ -77,7 +105,10 @@ int main(int argc, char *argv[])
 	}
 
 	free(head);
-	fclose(file);
+	fclose(file1);
+	if(argc == 3) {
+		fclose(file2);
+	}
 	free(line);
 	return(0);
 }
